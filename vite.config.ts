c1d3028566d56a59db410fb5a67ab626b9289ad0
Winwangit <160583089+Winwangit@users.noreply.github.com>
2025-11-14 12:20:38 +0300

    added homepage for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages base (case-sensitive repo name)
+
 export default defineConfig({
   plugins: [react()],
   base: '/Shopping-List/',
